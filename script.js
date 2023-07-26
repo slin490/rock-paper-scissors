f42e6f5034957452783277808df4ddb16fd90d81
Steve <lysoll8444@gmail.com>
2023-07-26 16:02:36 -0500

    pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ask user input
+// Take input and compare with computer
+// assign objects to numbers so computer can randomize
+// use random function to choose between 1-3
+// compare your input with computer
+// create conditional statements
+// points tracker
+
