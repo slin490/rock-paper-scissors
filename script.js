eabce4240f7f1e0bff3d06b1e39224b667d50d2f
Steve <lysoll8444@gmail.com>
2023-07-26 16:42:07 -0500

    varible-change

--- script.js
+++ script.js
@@ -12,30 +12,31 @@ const readline = require('readline-sync');
 const playerChoice = readline.question("rock, paper, or scissors? ").toLowerCase();
 const rpsChoices = ["rock", "paper", "scissors"];
 
-
 //Computer choice maker
-let compChoice = (listChoices) => {
+const compDecider = (listChoices) => {
 	const randNum = Math.floor(Math.random() * 3);
 	const choice = listChoices[randNum];
 	return choice;
 }
 
-if (playerChoice == compChoice(rpsChoices)){
+const compChoice = compDecider(rpsChoices);
+
+if (playerChoice == compChoice){
 	console.log("Tie");
 } else if (playerChoice == "rock"){
-	if (compChoice(rpsChoices) == "scissors"){
+	if (compChoice == "scissors"){
 		console.log("Player Wins")
 	} else {
 		console.log("Computer Wins")
 	}
 } else if (playerChoice == "paper"){
-	if (compChoice(rpsChoices) == "rock"){
+	if (compChoice == "rock"){
 		console.log("Player Wins")
 	} else {
 		console.log("Computer Wins")
 	}
 } else if (playerChoice == "scissors"){
-	if (compChoice(rpsChoices) == "paper"){
+	if (compChoice == "paper"){
 		console.log("Player Wins")
 	} else {
 		console.log("Computer Wins")
