62322550cb638c89548d68c56f64ecda551dd42d
Steve <lysoll8444@gmail.com>
2023-07-30 21:53:31 -0500

    rpsui-done

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
-const buttons = document.querySelectorAll("button");
-const winner = document.getElementById("notification");
-const playerPoints = document.getElementById("playerPoints");
-const botPoints = document.getElementById("computerPoints");
+const buttons = document.querySelectorAll(".choices");
+const playerText = document.getElementById("playerText");
+const computerText = document.getElementById("computerText");
+const resetBtn = document.querySelector("#reset");
+const winner = document.getElementById("winnerNote");
+let playerPoints = 0;
+let computerPoints = 0;
 
-buttons.forEach((button) => {
-	button.addEventListener("click", checkCondition);
-})
+//Computer Deicision
+const computerDecision = () => {
+	const randNumber = Math.floor(Math.random() * 3);
 
-const checkCondition = () => {
-	
+	switch(randNumber){
+		case 0:
+			computerChoice = "Rock";
+			break;
+		case 1:
+			computerChoice = "Paper";
+			break;
+		case 2:
+			computerChoice = "Scissors";
+			break;
+	}
 }
 
-// const compDecider = (listChoices) => {
-// 	const randNum = Math.floor(Math.random() * 3);
-// 	const choice = listChoices[randNum];
-// 	return choice;
-// }
+//Condition Checker
+const checkCondition = () => {
+	if (playerChoice == computerChoice){
+		console.log("Tie");
+	} else if (playerChoice == "Rock"){
+		return computerChoice == "Scissors" ? playerPoints += 1 : computerPoints += 1;
+	} else if (playerChoice == "Paper"){
+		return computerChoice == "Rock" ? playerPoints += 1 : computerPoints += 1;
+	} else if (playerChoice == "Scissors"){
+		return computerChoice == "Paper" ? playerPoints += 1 : computerPoints += 1;
+	}
+}
 
-// const compChoice = compDecider(rpsChoices);
+//First to Reach Five Points
+const checkWinner = () => {
+	if(playerPoints == 5){
+		winner.textContent = "Player Wins";
+	} else if (computerPoints == 5) {
+		winner.textContent = "Computer Wins";
+	}
+}
 
-// console.log(`You chose ${playerChoice}`);
-// console.log(`Computer chose ${compChoice}\n`)
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		playerChoice = button.textContent;
+		// console.log(playerChoice);
+		computerDecision();
+		// console.log(computerChoice);
+		checkCondition();
+		playerText.textContent = playerPoints;
+		computerText.textContent = computerPoints;
+		checkWinner();
+	});
+})
 
-// if (playerChoice == compChoice){
-// 	console.log("Tie");
-// } else if (playerChoice == "rock"){
-// 	if (compChoice == "scissors"){
-// 		console.log("Player Wins");
-// 		playerPoints += 1;
-// 	} else {
-// 		console.log("Computer Wins");
-// 		compPoints += 1;
-// 	}
-// } else if (playerChoice == "paper"){
-// 	if (compChoice == "rock"){
-// 		console.log("Player Wins");
-// 		playerPoints += 1;
-// 	} else {
-// 		console.log("Computer Wins");
-// 		compPoints += 1;
-// 	}
-// } else if (playerChoice == "scissors"){
-// 	if (compChoice == "paper"){
-// 		console.log("Player Wins");
-// 		playerPoints += 1;
-// 	} else {
-// 		console.log("Computer Wins");
-// 		compPoints += 1;
-// 	}
-// }
+const resetGame = () => {
+	playerPoints = 0;
+	computerPoints = 0;
+	playerText.textContent = playerPoints;
+	computerText.textContent = computerPoints;
+	winner.textContent = "Default Winner";
+}
+
+resetBtn.addEventListener("click", resetGame);
+
+// buttons.forEach((button) => {
+// 	button.addEventListener("click", () => {
+// 		playerText.textContent = button.textContent;
+// 	})
+// })
 
 
